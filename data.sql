5089a1ae17dc436aee701c932e1054625b953746
Isabella Otoo <isabellaotoo25@gmail.com>
2023-06-13 10:52:20 +0000

    Add one last record

--- data.sql
+++ data.sql
@@ -22,3 +22,5 @@ VALUES ('Devimon', '2017-05-12', 5, true, 11);
  VALUES ('Boarmon', '2005-06-07', 7, true, 20.4 );
  INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
  VALUES ('Blossom', '1998-10-13', 3, true, 17 );
+ INSERT INTO animals (name, date_of_birth, escape_attempts, neutered, weight_kg)
+ VALUES ('Ditto', '2022-05-14', 4, true, 22 );
