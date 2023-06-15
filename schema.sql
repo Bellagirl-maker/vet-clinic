0b80c916379a0852c840e54f1e2da330e8926434
Isabella Otoo <isabellaotoo25@gmail.com>
2023-06-15 03:17:12 -0700

    Create vet and the joining tables

--- schema.sql
+++ schema.sql
@@ -28,3 +28,27 @@ ALTER TABLE animals DROP COLUMN species;
 ALTER TABLE animals ADD COLUMN species_id INTEGER REFERENCES species(id);
 ALTER TABLE animals ADD COLUMN owner_id INTEGER REFERENCES owners(id);
 
+CREATE TABLE vets (
+	id INT GENERATED ALWAYS AS IDENTITY NOT NULL,
+	name VARCHAR(255),
+	age INT,
+	date_of_graduation date,
+	PRIMARY KEY(id)
+	);
+
+CREATE TABLE specializations (
+  vet_id INTEGER REFERENCES vets (id),
+  species_id INTEGER REFERENCES species (id),
+  PRIMARY KEY (vet_id, species_id)
+);
+
+CREATE TABLE visits (
+  vet_id INTEGER REFERENCES vets (id),
+  animal_id INTEGER REFERENCES animals (id),
+  visit_date DATE,
+  PRIMARY KEY (vet_id, animal_id)
+);
+
+
+
+
